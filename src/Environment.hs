c4f34a62786d3193e174b9657f9341d75e0e8566
Gardy <jef@versavel.be>
2020-11-08 17:39:57 +0100

    refactored 2d environment with Moore and Von Meumann neighbourhoods

--- src/Environment.hs
+++ src/Environment.hs
@@ -1,23 +1,89 @@
+{-# LANGUAGE GADTs #-}
+
 module Environment where
 
-import qualified Data.Map as M
+import Data.Matrix
+import Data.Maybe
+import qualified Data.Set as Set
+import Organism
+import Seeding
+import System.Random
+import Test.QuickCheck
+import Test.QuickCheck.Random
 
 type Resource = Double
 
 type Pos = (Int, Int)
 
-data Place a = Nil | Org a | Res Resource
+data Place a = Organism a => Nil | Org a | Res Resource
 
-type Env a = M.Map Int (M.Map Int (Place a))
+data Environment a = Organism a => Env (Matrix (Place a)) Neighbourhood Pos
 
-getAtPos :: Pos -> Env a -> Maybe (Place a)
-getAtPos (x, y) e1 = do
-  e2 <- M.lookup x e1
-  M.lookup y e2
+-- https://en.wikipedia.org/wiki/Moore_neighborhood
+-- https://en.wikipedia.org/wiki/Von_Neumann_neighborhood
+data Neighbourhood = Moore | VonNeumann
 
-insertAtPos :: Place a -> Pos -> Env a -> Maybe (Env a)
-insertAtPos p (x, y) e1 = do
-  e2 <- M.lookup x e1
-  let e2' = M.insert y p e2
-  return $ M.insert x e2' e1
+legalPos :: Pos -> Pos -> Bool
+legalPos (x, y) (mx, my) = x >= 1 && x <= mx && y >= 1 && y <= my
 
+legalLimits :: Pos -> Bool
+legalLimits (x, y) = x >= 0 && y >= 0
+
+getAllPos :: Pos -> [Pos]
+getAllPos (mx, my) = [(x, y) | x <- [1 .. mx], y <- [1 .. my]]
+
+getOrg :: Organism a => Place a -> Maybe a
+getOrg (Org o) = Just o
+getOrg _ = Nothing
+
+isOrg :: Organism a => Place a -> Bool
+isOrg (Org o) = True
+isOrg _ = False
+
+isNil :: Organism a => Place a -> Bool
+isNil Nil = True
+isNil _ = False
+
+getplaceAt :: Organism a => Environment a -> Pos -> Maybe (Place a)
+getplaceAt (Env m _ maxPos) p
+  | legalPos p maxPos = Just $ uncurry unsafeGet p m
+  | otherwise = Nothing
+
+nilGenerator :: Organism a => Pos -> Place a
+nilGenerator _ = Nil
+
+insertOrganismAt :: Organism a => Environment a -> a -> Pos -> Environment a
+insertOrganismAt (Env m n lim) org p
+  | legalPos p lim = Env (unsafeSet (Org org) p m) n lim
+  | otherwise = error "given position is not inbounds"
+
+adjacentPos :: Pos -> [Pos]
+adjacentPos (x, y) = [(x -1, y), (x + 1, y), (x, y -1), (x, y + 1)]
+
+diagonalPos :: Pos -> [Pos]
+diagonalPos (x, y) = [(x -1, y -1), (x + 1, y + 1), (x + 1, y -1), (x -1, y + 1)]
+
+getNeighbours :: Organism a => Environment a -> Pos -> [Pos]
+getNeighbours (Env _ Moore lim) p =
+  [neighbour | neighbour <- adjacentPos p ++ diagonalPos p, legalPos neighbour lim]
+getNeighbours (Env _ VonNeumann lim) p =
+  [neighbour | neighbour <- adjacentPos p, legalPos neighbour lim]
+
+getAllOrgs :: Organism a => Environment a -> [a]
+getAllOrgs (Env m _ _) = map (fromJust . getOrg) $ filter isOrg $ toList m
+
+empty :: Organism a => Pos -> Neighbourhood -> Environment a
+empty (mx, my) n
+  | legalLimits (mx, my) = Env (matrix mx my nilGenerator) n (mx, my)
+  | otherwise = error "the given limits must be positive and nonzero"
+
+distributeOrgs :: Organism a => QCGen -> [a] -> Pos -> IO [(Pos, a)]
+distributeOrgs gen orgs lim = distributeOrgs' gen orgs $ Set.fromList $ getAllPos lim
+
+distributeOrgs' :: Organism a => QCGen -> [a] -> Set.Set Pos -> IO [(Pos, a)]
+distributeOrgs' gen (o : os) set = do
+  let (g1, g2) = split gen
+  p <- generate $ useSeed g1 $ elements $ Set.toList set
+  let s = Set.delete p set
+  rest <- distributeOrgs' g2 os s
+  return $ (p, o) : rest
