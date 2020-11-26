06d2fc9d37d756cc43f49dbb3b90b5dee9926d8f
Gardy <jef@versavel.be>
2020-11-26 17:32:44 +0100

    Started on the rules of the resources in the system.

---
+++ src/Resource.hs
@@ -0,0 +1,22 @@
+module Resource where
+
+import Environment
+import Seeding
+import Test.QuickCheck
+import Test.QuickCheck.Random
+
+-- This module contains some functions to handle the resources
+-- and define some rules for the resources in the environment.
+
+-- | Returns the amount of total resources there are in the environment
+-- relative to the amount of cells in the environment.
+totalResources :: Double
+totalResources = 1
+
+-- | Returns whether the organism places its outcome into one of the neighbouring cells.
+-- When False the outcome is placed on the cell of the orgamism itself.
+neighbourDistribution :: Bool
+neighbourDistribution = True
+
+distributeResource :: QCGen -> [Pos] -> IO Pos
+distributeResource gen positions = generate $ useSeed gen $ elements positions
