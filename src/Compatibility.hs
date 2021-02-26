9383a69ea6031cae5e149815849e54749969d2a5
Gardy <jef@versavel.be>
2021-02-26 17:35:40 +0100

    added my own difference algorithm which seems to be easier to work with. I could not find a function in treediff that returned a number indicating the difference

--- src/Compatibility.hs
+++ src/Compatibility.hs
@@ -6,27 +6,28 @@ import Data.TreeDiff
 import qualified Generators as G
 
 compTest = do
-  trees <- G.randomGenerationTest
-  print trees
-  let first = head trees
-  let second = trees !! 1
+  let first = BinOp Add (Const 1) (Const 2)
+  let second = BinOp Add (Const 1) (Param 3)
   let d = ediff first second
-  print $ prettyEditExpr d
-  print $ calculateDifference first second
   print (size first + size second)
-  print $ differencePercentage first second
+  print $ matchingPercentage first second
+  let diff = getDiff first second * 2
+  print diff
+  print $ diff % (getNrNodes first + getNrNodes second)
 
 calculateDifference :: ASTExpression -> ASTExpression -> Int
 calculateDifference l r = fromIntegral (editDiff diffTree) `div` 2
   where
     diffTree = ediff l r
 
-differencePercentage :: ASTExpression -> ASTExpression -> Ratio Int
-differencePercentage l r = 1 - (calculateDifference l r % (size l + size r))
+matchingPercentage :: ASTExpression -> ASTExpression -> Ratio Int
+matchingPercentage l r = 1 - ((getDiff l r * 2) % (size l + size r))
 
 editDiff :: Edit EditExpr -> Int
 editDiff (Ins e) = editExprDiff e
 editDiff (Del e) = editExprDiff e
+editDiff (Cpy (EditApp _ l)) = sum $ map editDiff l
+editDiff (Cpy (EditLst l)) = sum $ map editDiff l
 editDiff (Cpy _) = 0
 editDiff (Swp l r) = editExprDiff l + editExprDiff r
 
@@ -38,3 +39,57 @@ editExprDiff :: EditExpr -> Int
 editExprDiff (EditApp _ l) = sum $ map editDiff l
 editExprDiff (EditLst l) = sum $ map editDiff l
 editExprDiff (EditExp e) = diff e
+
+class Difference a where
+  getDiff :: a -> a -> Int
+
+instance Difference ASTExpression where
+  getDiff (Const l) (Const r)
+    | l == r = 0
+    | otherwise = 2
+  getDiff (Const _) l = size l
+  getDiff r (Const _) = size r
+  getDiff (Param l) (Param r)
+    | l == r = 0
+    | otherwise = 2
+  getDiff (Param _) l = size l
+  getDiff r (Param _) = size r
+  getDiff (BinOp opl ll lr) (BinOp opr rl rr) =
+    getDiff opl opr
+      + getDiff ll rl
+      + getDiff lr rr
+  -- + minimum [getDiff ll rl + getDiff lr rr, 1 + getDiff ll rr + getDiff lr rl]
+  getDiff (BinOp _ ll lr) (RelOp _ rl rr) =
+    1
+      + getDiff ll rl
+      + getDiff lr rr
+  -- + minimum [getDiff ll rl + getDiff lr rr, 1 + getDiff ll rr + getDiff lr rl]
+  getDiff (BinOp _ ll lr) (UnOp _ rl) =
+    1
+      + minimum [getDiff ll rl, getDiff lr rl]
+  getDiff (RelOp opl ll lr) (RelOp opr rl rr) =
+    getDiff opl opr
+      + getDiff ll rl
+      + getDiff lr rr
+  -- + minimum [getDiff ll rl + getDiff lr rr, 1 + getDiff ll rr + getDiff lr rl]
+  getDiff (RelOp opl ll lr) (BinOp opr rl rr) = getDiff (BinOp opr rl rr) (RelOp opl ll lr)
+  getDiff (RelOp _ ll lr) (UnOp _ rl) =
+    1 + minimum [getDiff ll rl, getDiff lr rl]
+  getDiff (UnOp opl l) (UnOp opr r) = getDiff opl opr + getDiff r l
+  getDiff (UnOp opl ll) (BinOp opr rl rr) = getDiff (BinOp opr rl rr) (UnOp opl ll)
+  getDiff (UnOp opl ll) (RelOp opr rl rr) = getDiff (RelOp opr rl rr) (UnOp opl ll)
+
+instance Difference BinaryOperation where
+  getDiff l r
+    | l == r = 0
+    | otherwise = 1
+
+instance Difference UnaryOperation where
+  getDiff l r
+    | l == r = 0
+    | otherwise = 1
+
+instance Difference RelationalOperation where
+  getDiff l r
+    | l == r = 0
+    | otherwise = 1
