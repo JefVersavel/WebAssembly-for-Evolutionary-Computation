fe49c0f97dac8a76d16562aca7bc24f361dc5348
Gardy <jef@versavel.be>
2021-04-02 14:42:36 +0200

    implemented the imported functions in the generators

--- src/Generators.hs
+++ src/Generators.hs
@@ -2,6 +2,7 @@ module Generators where
 
 import AST
 import Control.Monad.Reader
+import ImportedFunction
 import Seeding
 import System.Random
 import Test.QuickCheck
@@ -80,13 +81,33 @@ rIf gC gL gR = do
   l <- gL
   IfStatement c l <$> gR
 
+rImportedFunction ::
+  ImportedFunctions ->
+  Algo ->
+  Depth ->
+  NrParam ->
+  Gen ASTExpression
+rImportedFunction list algo depth params = do
+  (ImportedFunction name nrParam) <- elements list
+  ImportedFunctionCall name <$> generateExprList algo depth params list nrParam
+
+type Algo = Depth -> Reader (Depth, NrParam, ImportedFunctions) (Gen ASTExpression)
+
+generateExprList :: Algo -> Depth -> NrParam -> ImportedFunctions -> Int -> Gen [ASTExpression]
+generateExprList _ _ _ _ 0 = return []
+generateExprList algo depth params functions n = do
+  generated <- runReader (algo 0) (depth, params, functions)
+  rest <- generateExprList algo depth params functions $ n -1
+  return $ generated : rest
+
 -- | GROW algorithm that builds a generator for an ASTExpression.
 -- As long as the depth is shorter than the given maximum depth all possible nodes can be chosen.
 -- Once the given maximum epth is reached only leaf nodes an be chosen such as a constant or a parameter.
 -- The maximum depth and the number of parameters are registered using the Reader monad.
-growOne :: Depth -> Reader (Depth, NrParam) (Gen ASTExpression)
+growOne :: Depth -> Reader (Depth, NrParam, ImportedFunctions) (Gen ASTExpression)
 growOne d = do
-  (maxD, nrParam) <- ask
+  (maxD, nrParam, functions) <- ask
+  let len = length functions
   if d < maxD
     then do
       g1 <- growOne $ d + 1
@@ -102,12 +123,13 @@ growOne d = do
             (1, rGlobalTee g1),
             (1, rGlobalGet),
             (1, rGlobalSet g1 g2),
-            (1, rIf g1 g2 g3)
+            (1, rIf g1 g2 g3),
+            (len, rImportedFunction functions growOne (d + 1) nrParam)
           ]
     else return $ oneof [rConst, rParam nrParam, rGlobalGet]
 
 -- | Helper function for the GROW algorithm to set the initial QCGen.
-growOneInit :: QCGen -> Reader (Depth, NrParam) (Gen ASTExpression)
+growOneInit :: QCGen -> Reader (Depth, NrParam, ImportedFunctions) (Gen ASTExpression)
 growOneInit gen = do
   g <- growOne 0
   let seeded = useSeed gen g
@@ -117,25 +139,29 @@ growOneInit gen = do
 -- As long as the depth is shorter than the given maximum depth only non-leaf nodes can be selected.
 -- Once the given maximum depth is reached only leaf nodes can be chosen such as a constant or a parameter.
 -- The maximum depth and the number of parameters are registered using the Reader monad.
-fullOne :: Depth -> Reader (Depth, NrParam) (Gen ASTExpression)
+fullOne :: Depth -> Reader (Depth, NrParam, ImportedFunctions) (Gen ASTExpression)
 fullOne d = do
-  (maxD, nrParam) <- ask
+  (maxD, nrParam, functions) <- ask
+  let len = length functions
   if d < maxD
     then do
       f1 <- fullOne $ d + 1
       f2 <- fullOne $ d + 1
+      f3 <- fullOne $ d + 1
       return $
         frequency
           [ (length [minBound :: BinaryOperation ..], rBinExpr f1 f2),
             (length [minBound :: UnaryOperation ..], rUnExpr f1),
             (length [minBound :: RelationalOperation ..], rRelExpr f1 f2),
             (10, rGlobalTee f1),
-            (1, rGlobalSet f1 f2)
+            (1, rGlobalSet f1 f2),
+            (1, rIf f1 f2 f3),
+            (len, rImportedFunction functions growOne (d + 1) nrParam)
           ]
     else return $ oneof [rConst, rParam nrParam, rGlobalGet]
 
 -- | Helper function for the FULL algorithm to set the initial QCGen.
-fullOneInit :: QCGen -> Reader (Depth, NrParam) (Gen ASTExpression)
+fullOneInit :: QCGen -> Reader (Depth, NrParam, ImportedFunctions) (Gen ASTExpression)
 fullOneInit gen = do
   g <- fullOne 0
   let seeded = useSeed gen g
@@ -143,8 +169,8 @@ fullOneInit gen = do
 
 -- | Returns a list of generators for expressions given an initial QCGen and a ratio of generators
 -- that were conceived using the GROW and FULL method.
-rampedHalfNHalf :: QCGen -> Depth -> NrParam -> Ratio -> Size -> [Gen ASTExpression]
-rampedHalfNHalf gen d nrParam ratio n
+rampedHalfNHalf :: QCGen -> Depth -> NrParam -> Ratio -> Size -> ImportedFunctions -> [Gen ASTExpression]
+rampedHalfNHalf gen d nrParam ratio n functions
   | ratio <= 1 && ratio >= 0 = growList ++ fullList
   | otherwise = error "Please provide a ratio between 0 and 1."
   where
@@ -154,14 +180,14 @@ rampedHalfNHalf gen d nrParam ratio n
     growList = generateInitList growSeeds growOneInit
     fullList = generateInitList fullSeeds fullOneInit
     generateInitList gens method =
-      [runReader (method g) (fst $ randomR (1, d) g, nrParam) | g <- gens]
+      [runReader (method g) (fst $ randomR (1, d) g, nrParam, functions) | g <- gens]
 
 -- | Returns a list of generators given the initial seed, maximium depth, number of paramets.
 -- ratio of GROW and FULL and the lenght of the list of generators.
 -- The list of gnerators are conceived using rampedHalfNHalf.
-genASTExpressions :: Seed -> Depth -> NrParam -> Ratio -> Size -> IO [ASTExpression]
-genASTExpressions seed d nrParam ratio n = do
+genASTExpressions :: Seed -> Depth -> NrParam -> Ratio -> Size -> ImportedFunctions -> IO [ASTExpression]
+genASTExpressions seed d nrParam ratio n functions = do
   sequence
-    [generate g | g <- rampedHalfNHalf (mkQCGen seed) d nrParam ratio n]
+    [generate g | g <- rampedHalfNHalf (mkQCGen seed) d nrParam ratio n functions]
 
 randomGenerationTest = genASTExpressions 12 6 1 0.5 10
