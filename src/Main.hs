e6c1c02319e085f625526274ddb147bcb4fd16e6
Gardy <jef@versavel.be>
2021-05-18 10:23:24 +0200

    test for first generators test

--- src/Main.hs
+++ src/Main.hs
@@ -10,7 +10,7 @@ seeds = [1 .. 10]
 ancestorCombos :: [(ASTExpression, Double)]
 ancestorCombos = [(ancestor1, start1)]
 
-generators = [(generator1, generatorStart1)]
+generators = [(generator1, generatorStart1), (generator2, generatorStart2)]
 
 limitations :: [Int]
 limitations = [10]
@@ -21,9 +21,9 @@ mutationRates = [2 .. 10]
 main :: IO [()]
 main = do
   sequence
-    [ mainCreature seed ancestor1 gen start1 st 10000 5 10 5 m a
+    [ mainCreature seed ancestor1 gen start1 st 100 5 10 5 m a
       | seed <- seeds,
         (gen, st) <- generators,
-        m <- [1 .. 3],
-        a <- [0 .. 2]
+        m <- [1 .. 5],
+        a <- [0 .. 5]
     ]
