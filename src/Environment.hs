29d0160ff46b0a87c64c8a2dfec7fa0fa0c3399f
Gardy <jef@versavel.be>
2021-05-18 09:53:48 +0200

    fixed typo

--- src/Environment.hs
+++ src/Environment.hs
@@ -456,7 +456,7 @@ initializeEnvironment n gen orgList lim start = do
   let amount = floor (fromIntegral (getSize env) :: Double)
   let resources = generateResources g1 amount 0
   let (g21, g22) = R.split g2
-  let extraRes = generateResourcesFixed g21 (length posList) start
+  let extraRes = generateResourcesFixed g21 (length posList) $ start * 2
   posRes <- distribute g22 resources lim
   let newEnv = fillInResources env posRes
   let extraResEnv = fillInResources newEnv $ zip posList extraRes
